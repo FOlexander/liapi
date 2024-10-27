2fc520a7bf34b66272e73702253197e977e297e7
Olexander <afurmanenko@gmail.com>
2024-10-27 16:57:25 +0200

    add python-dotenv for saving password

--- lidata.py
+++ lidata.py
@@ -1,9 +1,15 @@
-from linkedin_api import Linkedin
+import os
 import json
+from dotenv import load_dotenv
+from linkedin_api import Linkedin
+
+load_dotenv()  
+mail = os.getenv("MAIL_F")
+password = os.getenv("PASSWORD_F")
 
 def get_profile_data(profile_id):
     # Authenticate using any Linkedin user account credentials
-    api = Linkedin('prodigy86@ex.ua', 'Karandash17')
+    api = Linkedin(mail, password)
 
     profile_id = profile_id.split("/")[-2]
     profile_data = api.get_profile(profile_id)
